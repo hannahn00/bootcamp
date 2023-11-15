d0bcb25d35dd995256f1247fe7997d9277179b77
Hannah Nguyen <hannahng004@gmail.com>
2023-11-14 20:25:30 -0500

    part3

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom';
 class Homepage extends React.Component {
     constructor(props) {
         super(props);
+        // this.state = {};
     };
+
 
     render () {
         return (
